8d5efe7a56ae887f578cad6df91c8ec024f5a61d
Fantine Huot <fantine.huot@gmail.com>
2021-04-02 19:17:50 +0200

    Add residual network

--- model/__init__.py
+++ model/__init__.py
@@ -2,3 +2,5 @@ from .cnn1d_example import CNN1DExample
 from .cnn2d_example import CNN2DExample
 from .cnn1d_modular import CNN1DModular
 from .cnn2d_modular import CNN2DModular
+from .cnn1d_residual import CNN1DResidual
+from .cnn2d_residual import CNN2DResidual
