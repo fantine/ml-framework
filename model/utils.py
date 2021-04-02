87e6a8b27df73254242ac33134be70a548d8e2cd
Fantine Huot <fantine.huot@gmail.com>
2021-04-02 17:39:16 +0200

    Refactor common functions into utils

--- model/utils.py
+++ model/utils.py
@@ -20,3 +20,97 @@ def get_regularizer(regularizer=0, reg_weight=1e-4):
     return keras.regularizers.l1(reg_weight)
   if regularizer == 2:
     return keras.regularizers.l2(reg_weight)
+
+
+def residual_block_1d(inputs, filters, strides, regularizer):
+  """Creates 1D convolution layer block with residual connections.
+
+  Args:
+    inputs: Input to the residual block.
+    filters: List of filters to use in successive layers.
+    strides: List of strides to use in successive layers.
+    regularizer: Regularizer.
+
+  Returns:
+    Output of the residual block.
+  """
+  x = keras.layers.BatchNormalization()(inputs)
+  x = keras.layers.LeakyReLU()(x)
+  if strides[0] == 1:
+    x = keras.layers.Conv1D(
+        filters=filters[0],
+        kernel_size=3,
+        strides=1,
+        padding='same',
+        use_bias=False,
+        regularizer=regularizer)(x)
+  else:
+    x = keras.layers.MaxPooling1D(pool_size=2, strides=strides[0])(x)
+
+  x = keras.layers.BatchNormalization()(x)
+  x = keras.layers.LeakyReLU()(x)
+  x = keras.layers.Conv1D(
+      filters=filters[1],
+      kernel_size=3,
+      strides=strides[1],
+      padding='same',
+      use_bias=False,
+      regularizer=regularizer)(x)
+  # Construct the residual link that bypasses this block.
+  shortcut = keras.layers.Conv1D(
+      filters=filters[1],
+      kernel_size=1,
+      strides=strides[0],
+      padding='same'
+      use_bias=False,
+      regularizer=regularizer)(inputs)
+  shortcut = keras.layers.BatchNormalization()(shortcut)
+
+  return shortcut + x
+
+
+def residual_block_2d(inputs, filters, strides, regularizer):
+  """Creates 2D convolution layer block with residual connections.
+
+  Args:
+    inputs: Input to the residual block.
+    filters: List of filters to use in successive layers.
+    strides: List of strides to use in successive layers.
+    regularizer: Regularizer.
+
+  Returns:
+    Output of the residual block.
+  """
+  x = keras.layers.BatchNormalization()(inputs)
+  x = keras.layers.LeakyReLU()(x)
+  if strides[0] == 1:
+    x = keras.layers.Conv2D(
+        filters=filters[0],
+        kernel_size=3,
+        strides=1,
+        padding='same',
+        use_bias=False,
+        regularizer=regularizer)(x)
+  else:
+    x = keras.layers.MaxPooling2D(pool_size=2, strides=strides[0])(x)
+
+  x = keras.layers.BatchNormalization()(x)
+  x = keras.layers.LeakyReLU()(x)
+  x = keras.layers.Conv2D(
+      filters=filters[1],
+      kernel_size=3,
+      strides=strides[1],
+      padding='same',
+      use_bias=False,
+      regularizer=regularizer)(x)
+  # Construct the residual link that bypasses this block.
+  shortcut = keras.layers.Conv2D(
+      filters=filters[1],
+      kernel_size=1,
+      strides=strides[0],
+      padding='same'
+      use_bias=False,
+      regularizer=regularizer)(inputs)
+  shortcut = keras.layers.BatchNormalization()(shortcut)
+
+  return shortcut + x
